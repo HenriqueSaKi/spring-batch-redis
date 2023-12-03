7926bab4046be54df9c81eefa5c34c3d57313fdb
Henrique Kisaki <satoshikisaki@hotmail.com>
2023-12-03 16:21:27 -0300

    First project commit

---
+++ spring-batch-redis/src/main/java/br/com/springbatchredis/SpringBatchRedisApplication.java
@@ -0,0 +1,13 @@
+package br.com.springbatchredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchRedisApplication.class, args);
+	}
+
+}
