1fc516e9bd27691b8a325bdd74effc27e82f6942
Henrique Kisaki <satoshikisaki@hotmail.com>
2023-12-21 23:51:24 -0300

    Job scheduled to run every 5 seconds

--- spring-batch-redis/src/main/java/br/com/springbatchredis/config/BatchConfig.java
+++ spring-batch-redis/src/main/java/br/com/springbatchredis/config/BatchConfig.java
@@ -1,55 +1,74 @@
 package br.com.springbatchredis.config;
 
 import br.com.springbatchredis.model.ReaderCSVModel;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfig {
 
-    @Autowired
-    private JobBuilderFactory jobBuilderFactory;
+    private final Logger logger = LoggerFactory.getLogger(BatchConfig.class);
+    @Autowired private DataSource dataSource;
+    @Autowired private JobLauncher jobLauncher;
+    @Autowired private JobRepository jobRepository;
+    @Autowired private PlatformTransactionManager transactionManager;
+    @Autowired private JobBuilderFactory jobBuilderFactory;
+    @Autowired private StepBuilderFactory stepBuilderFactory;
 
-    @Autowired
-    private StepBuilderFactory stepBuilderFactory;
+    @Scheduled(fixedRate = 5000) //Execute job every 5 seconds
+    public void launchJob () throws Exception {
+        logger.info("Batch processing starts at: " + LocalDateTime.now().toString());
+        JobExecution jobExecution =
+                jobLauncher.run(job(jobRepository, transactionManager),
+                        new JobParametersBuilder().addDate("launchJob", new Date()).toJobParameters());
+        logger.info("Batch processing ends at: " + LocalDateTime.now().toString());
+    }
 
     @Bean
-    public Job job(Step step) {
+    public Job job(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return jobBuilderFactory
                 .get("job")
-                .start(step)
+                .start(step(jobRepository, transactionManager))
                 .incrementer(new RunIdIncrementer())
                 .build();
     }
 
     @Bean
-    public Step step (ItemReader<ReaderCSVModel> reader, ItemWriter<ReaderCSVModel> writer) {
+    public Step step (JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return stepBuilderFactory
             .get("step")
             .<ReaderCSVModel, ReaderCSVModel>chunk(5)
-            .reader(reader)
-            .writer(writer)
+            .reader(reader())
+            .writer(writer())
             .build();
     }
 
     @Bean
-    public ItemReader<ReaderCSVModel> readCSVFile() {
+    public ItemReader<ReaderCSVModel> reader() {
         return new FlatFileItemReaderBuilder<ReaderCSVModel>()
             .name("reader")
             .resource(new FileSystemResource("src/main/resources/dataset/100-random-datas.csv"))
@@ -62,7 +81,7 @@ public class BatchConfig {
     }
 
     @Bean
-    public ItemWriter<ReaderCSVModel> writer(DataSource dataSource) {
+    public ItemWriter<ReaderCSVModel> writer() {
         return new JdbcBatchItemWriterBuilder<ReaderCSVModel>()
             .dataSource(dataSource)
             .sql("INSERT INTO CSV_DATA (ID, NAME, DATE_OF_BIRTHDAY, CURRENT_JOB) " +
--- spring-batch-redis/src/main/java/br/com/springbatchredis/service/impl/ReaderServiceCSVImpl.java
+++ spring-batch-redis/src/main/java/br/com/springbatchredis/service/impl/ReaderServiceCSVImpl.java
@@ -1,15 +1,12 @@
 package br.com.springbatchredis.service.impl;
 
 import br.com.springbatchredis.model.ReaderCSVModel;
-import br.com.springbatchredis.model.ReaderTXTModel;
 import br.com.springbatchredis.repository.ReaderCSVRepository;
-import br.com.springbatchredis.repository.ReaderTXTRepository;
 import br.com.springbatchredis.service.ReaderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
